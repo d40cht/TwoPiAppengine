63c6b646fb73fe2e02eb3acf82f87482ad42c3ba
Alex Wilson <alex_wilson@pobox.com>
2014-10-17 18:50:24 +0100

    Splash screen is now a controller.

---
+++ dart/lib/pages/splash-screen.dart
@@ -0,0 +1,10 @@
+part of twopi_controller;
+
+@Component(
+    selector: '[splash-screen]',
+    templateUrl: 'packages/twopi/pages/splash-screen.html',
+    cssUrl: 'packages/twopi/pages/splash-screen.css',
+    publishAs: 'cmp')
+class SplashScreenComponent {
+  String foo = "Hello";
+}
